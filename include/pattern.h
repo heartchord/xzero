d4e77edddb9efc0f8b07738dd53b781c12e51e61
hc <chrislue1986@hotmail.com>
2017-06-08 16:24:30 +0800

    compile test

--- include/pattern.h
+++ include/pattern.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "public.h"
+
+namespace xzero
+{
+    template <class T>
+    class KG_Singleton : private KG_UnConstructable
+    {
+    public:
+        static T& GetInstance()
+        {
+            static T l_sInstance;
+            return l_sInstance;
+        }
+    };
+
+    #define KG_SINGLETON_DECLARE(T) friend class xzero::KG_Singleton<T>
+    #define KG_SINGLETON_REF(T)     (xzero::KG_Singleton<T>::GetInstance())
+    #define KG_SINGLETON_PTR(T)     (&xzero::KG_Singleton<T>::GetInstance())
+}
--- include/public.h
+++ include/public.h
@@ -1,1 +1,24 @@
 #pragma once
+
+namespace xzero 
+{
+    class KG_UnCopyable
+    {
+    protected:
+        KG_UnCopyable() {}                                              // constructor
+        ~KG_UnCopyable() {}                                             // destructor
+    private:
+        KG_UnCopyable(const KG_UnCopyable &) {}                         // copy constructor
+        KG_UnCopyable &operator=(const KG_UnCopyable &) {}              // copy assignment
+    };
+
+    class KG_UnConstructable
+    {
+    protected:
+        ~KG_UnConstructable() {}                                        // destructor
+    private:
+        KG_UnConstructable() {}                                         // constructor
+        KG_UnConstructable(const KG_UnConstructable &) {}               // copy constructor
+        KG_UnConstructable &operator=(const KG_UnConstructable &) {}    // copy assignment
+    };
+}

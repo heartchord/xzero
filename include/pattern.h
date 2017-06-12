9c5d35d7e661ac875af27ad0361a710f365fad4e
hc <chrislue1986@hotmail.com>
2017-06-12 14:20:24 +0800

    compile test

--- include/pattern.h
+++ include/pattern.h
@@ -8,14 +8,27 @@ namespace xzero
     class KG_Singleton : private KG_UnConstructable
     {
     public:
-        static T& GetInstance()
+        static T& GetInstanceRef()
         {
             static T l_sInstance;
             return l_sInstance;
         }
+
+        static T* GetInstancePtr()
+        {
+            return &GetInstanceRef();
+        }
     };
 
-    #define KG_SINGLETON_DECLARE(T) friend class xzero::KG_Singleton<T>
-    #define KG_SINGLETON_REF(T)     (xzero::KG_Singleton<T>::GetInstance())
-    #define KG_SINGLETON_PTR(T)     (&xzero::KG_Singleton<T>::GetInstance())
+    #define KG_SINGLETON_DCL_STRICT(T)       \
+        friend class xzero::KG_Singleton<T>; \
+        private:                             \
+            T() {}                           \
+            T(const T &) {}                  \
+            T &operator=(const T &) {}       \
+        public:                              \
+            ~T() {}
+    #define KG_SINGLETON_DCL(T) friend class xzero::KG_Singleton<T>;
+    #define KG_SINGLETON_REF(T) xzero::KG_Singleton<T>::GetInstanceRef()
+    #define KG_SINGLETON_PTR(T) xzero::KG_Singleton<T>::GetInstancePtr()
 }
